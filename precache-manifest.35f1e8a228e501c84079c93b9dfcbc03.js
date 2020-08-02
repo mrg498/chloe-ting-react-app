aafafd0afc86f1362552e0082738882200ee3e6c
Miles Grossenbacher <mrg498@nyu.edu>
2020-08-02 12:50:42 -0400

    Updates

---
+++ precache-manifest.35f1e8a228e501c84079c93b9dfcbc03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40446a2a3938775f204d5c90aff10a1",
+    "url": "/chloe-ting-react-app/index.html"
+  },
+  {
+    "revision": "cce99d2fda4832329b65",
+    "url": "/chloe-ting-react-app/static/css/main.a92a83b3.chunk.css"
+  },
+  {
+    "revision": "86f95671e3da0a344be9",
+    "url": "/chloe-ting-react-app/static/js/2.942cec7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chloe-ting-react-app/static/js/2.942cec7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce99d2fda4832329b65",
+    "url": "/chloe-ting-react-app/static/js/main.eec0bed9.chunk.js"
+  },
+  {
+    "revision": "805f5a0a9703d95811aa",
+    "url": "/chloe-ting-react-app/static/js/runtime-main.7fae9d5e.js"
+  }
+]);
